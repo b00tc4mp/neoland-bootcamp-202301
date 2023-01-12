056b1151726d193df625076914eca0730d1bfdef
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2023-01-12 17:25:29 +0100

    add for, functions and while in node

---
+++ stuff/node/while.js
@@ -0,0 +1,27 @@
+// var n = 1;
+
+// while (n < 101) {
+//   console.log(n);
+
+//   n = n + 1;
+// }
+
+var whiskysAndBourbon = [
+  "jack daniels",
+  "jonny walker",
+  "j & b",
+  "mackallan",
+  "ballantines",
+];
+
+var n = 0;
+
+while (n < whiskysAndBourbon.length) {
+  console.log("😀 " + whiskysAndBourbon[n]);
+
+  n++;
+  //   n += 1
+  //   n = n + 1;
+}
+
+console.log("😵‍💫😵‍💫😵‍💫😵‍💫😵‍💫😵‍💫😵");
