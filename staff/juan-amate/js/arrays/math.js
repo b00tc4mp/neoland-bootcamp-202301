07bde87fa614ad69246ab0d0c3ee47033d72867a
amat3 <121510770+amat3@users.noreply.github.com>
2023-01-13 22:38:34 +0100

    math ok

--- staff/juan-amate/js/arrays/math.js
+++ staff/juan-amate/js/arrays/math.js
@@ -2,28 +2,23 @@
  * Returns the max value in the array of numbers
  *
  * @param {array} nums The array of numbers to be multiplicados
- * @param {number} multiplicador 
+ * @param {number} multiplicador
  * @return {array} array of numbers multiplicados
  */
+
+var value = [];
+
 function multiplicar(numbers, multiplicador) {
-    // TODO implement this
-    
-    var numbersMultiplicados = [];
-    
-    var num = numbers[2];
-    console.log(num);
-
-    /*
-    for (var i = 0; i < numbers.length; i++) {
-        var num = multiplicador * numbers[i];
-        numbersMultiplicados[i] = num;
-    } 
-    return numbersMultiplicados;
+  for (var i = 0; i < numbers.length; i++) {
+    value[i] = numbers[i] * multiplicador;
   }
-  
+  return value;
+}
 
-  var result = multiplicar([10, 60, 5], 2);
-  // output expectd = [20, 120, 10]
-  
-  var result = multiplicar([7, 3, 1], 10);
-  // output expectd = [70, 30, 10]
+var result = multiplicar([10, 60, 5], 2);
+console.log(result);
+// output expectd = [20, 120, 10]
+
+var result = multiplicar([7, 3, 1], 10);
+console.log(result);
+// output expectd = [70, 30, 10]
