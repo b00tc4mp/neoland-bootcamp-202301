8d35090a7193d509af727357da23192c34e67cd0
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2023-01-13 17:59:05 +0100

    add math.js excercise

---
+++ staff/federico-lopez/arrays/average.js
@@ -0,0 +1,20 @@
+/**
+ * Returns the average of an array of numbers
+ *
+ * @param {array} nums The numbers to extract the average from
+ * @return {number} The average of the numbers
+ */
+function average(nums) {
+  // TODO implement me
+}
+
+// tests
+
+var result = average([10, 20, 30]);
+console.log(result); // 20
+
+var result = average([0, 20, 30, 40, 50]);
+console.log(result); // 30
+
+var result = average([1.2, 3.4, 5.6, 7.8, 9.1]);
+console.log(result);
