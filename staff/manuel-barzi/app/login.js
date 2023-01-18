c78e3614c698142202d3466d7fe40f9a251750d0
manuelbarzi <manuelbarzi@gmail.com>
2023-01-18 11:04:27 +0100

    add register logic and transition to login

---
+++ staff/manuel-barzi/app/login.js
@@ -0,0 +1,5 @@
+var login = {}
+
+login.main = document.querySelector('.login')
+login.main.classList.add('off')
+login.form = login.main.querySelector('form')
--- staff/manuel-barzi/app/register.js
+++ staff/manuel-barzi/app/register.js
@@ -1,17 +1,41 @@
-var form = document.querySelector('form')
+var register = {}
 
-var nameInput = form.querySelector('input#name')
-var ageInput = form.querySelector('input#age')
-var emailInput = form.querySelector('input#email')
-var passwordInput = form.querySelector('input#password')
+register.main = document.querySelector('.register')
+register.form = register.main.querySelector('form')
+register.nameInput = register.form.querySelector('input#name')
+register.ageInput = register.form.querySelector('input#age') 
+register.emailInput = register.form.querySelector('input#email')
+register.passwordInput = register.form.querySelector('input#password')
 
-form.onsubmit = function(event) {
+register.feedback = register.main.querySelector('.feedback')
+register.feedback.classList.add('off')
+
+register.form.onsubmit = function(event) {
     event.preventDefault()
 
-    console.log(
-        nameInput.value,
-        ageInput.value,
-        emailInput.value,
-        passwordInput.value
-    )
+    var name = register.nameInput.value
+    var age = Number(register.ageInput.value)
+    var email = register.emailInput.value
+    var password = register.passwordInput.value
+
+    try {
+        registerUser(name, age, email, password)
+
+        register.form.reset()
+
+        register.main.classList.add('off')
+        login.main.classList.remove('off')
+    } catch(error) {
+        //alert(error.message)
+        register.feedback.innerText = error.message
+        register.feedback.classList.remove('off')
+    }
 }
+
+register.nameInput.onclick = function(event) {
+    register.feedback.classList.add('off')
+}
+
+register.ageInput.onclick = register.nameInput.onclick
+register.emailInput.onclick = register.nameInput.onclick
+register.passwordInput.onclick = register.nameInput.onclick
