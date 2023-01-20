9749e0911386d821036a8cc04c03361a2dd6baa7
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2023-01-20 15:00:20 +0100

    p

--- staff/federico-lopez/arrays/max.js
+++ staff/federico-lopez/arrays/max.js
@@ -1,22 +1,24 @@
 /**
  * Returns the max value in the array of numbers
- *
+ * 
  * @param {array} nums The array of numbers to get the max value from
  * @return {number} The max value
  */
 function max(nums) {
-  // TODO implement me
+    // TODO implement me
 }
 
 // TIP learn if-else control flow
 
 // tests
 
-var result = max([10, 20, 100, 80, 70]);
-console.log(result); // 100
+var result = max([10, 20, 100, 80, 70])
+console.log(result) // 100
 
-var result = max([1.2, 2.3, 1, 8.9, 7.8]);
-console.log(result); // 8.9
 
-var result = max([-10, -1, -100, 100, 1000, 1000, 800]);
-console.log(result); // 1000
+var result = max([1.2, 2.3, 1, 8.9, 7.8])
+console.log(result) // 8.9
+
+var result = max([-10, -1, -100, 100, 1000, 1000, 800])
+console.log(result) // 1000
+
