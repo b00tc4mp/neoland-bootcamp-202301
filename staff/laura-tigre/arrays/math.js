a0e92142120a4fd61d7d6a62381478433a2bfef9
LauraTigreCarrasco <lauratigre@gmail.com>
2023-01-20 18:58:13 +0100

    add news challenges

--- staff/laura-tigre/arrays/math.js
+++ staff/laura-tigre/arrays/math.js
@@ -1,27 +1,27 @@
 /**
  * Returns the max value in the array of numbers
  *
- * @param {array} nums The array of numbers to be multiplicados
+ * @param {array} numbers The array of numbers to be multiplicados
  * @param {number} multiplicador 
  * @return {array} array of numbers multiplicados
  */
 function multiplicar(numbers, multiplicador) {
-  var resultMult=[];
-   for(var i =0; i<numbers.length; i++){
+  var resultMult = [];
+
+  for (var i = 0; i < numbers.length; i++) {
     /**resultMult[i]= numbers[i]*multiplicador;**/
-    var nums= numbers[i];
-    var number= multiplicador;
-    resultMult[i]= nums* number;
-    
-   }
-  
-   return resultMult
+    var number = numbers[i];
+
+    resultMult[i] = number * multiplicador;
   }
 
-  var result = multiplicar([10, 60, 5], 2);
-  // output expectd = [20, 120, 10]
-  console.log(result)
-  
-  var result = multiplicar([7, 3, 1], 10);
-  // output expectd = [70, 30, 10]
-  console.log(result)
+  return resultMult
+}
+
+var result = multiplicar([10, 60, 5], 2);
+// output expectd = [20, 120, 10]
+console.log(result)
+
+var result = multiplicar([7, 3, 1], 10);
+// output expectd = [70, 30, 10]
+console.log(result)
