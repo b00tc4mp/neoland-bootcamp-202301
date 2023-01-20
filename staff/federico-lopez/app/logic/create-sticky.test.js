3e352521d301e6abe370323c58060304d1cef3c9
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2023-01-20 17:32:13 +0100

    add changes

--- staff/federico-lopez/app/logic/create-sticky.test.js
+++ staff/federico-lopez/app/logic/create-sticky.test.js
@@ -1,9 +1,35 @@
 // case 0
 
-createSticky('peter@pan.com', 'hello wendy', 'public')
-// console.log(stickies)
+var users = [{ email: "peter@pan.com", password: "123123123" }];
+var stickies = [];
+
+function createSticky(user, text, visibility) {
+  for (var i = 0; i < users.length; i++) {
+    var userInDb = users[i];
+
+    if (userInDb.email === user) {
+      var newSticky = {
+        user,
+        text,
+        visibility,
+      };
+
+      stickies.push(newSticky);
+
+      return;
+    }
+  }
+  throw new Error("user not found");
+}
+
+createSticky("peter@pan.com", "hello wendy", "public");
+console.log(stickies);
 
 // case 1
 
-createSticky('wendy@pdarling.com', 'hello peter ,)', 'public')
-// console.log(stickies)
+try {
+  createSticky("wendy@pdarling.com", "hello peter ,)", "public");
+} catch (error) {
+  console.log("it threw a correct error");
+  console.log(stickies);
+}
