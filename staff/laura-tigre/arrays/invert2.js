157d97a5f34aa5ad0c04ed1f0fa09b87b0a06273
LauraTigreCarrasco <lauratigre@gmail.com>
2023-01-23 19:01:05 +0100

    practica de invert.js

---
+++ staff/laura-tigre/arrays/invert2.js
@@ -0,0 +1,44 @@
+/**
+ * Inverts the elements of an array into a new array
+ * It is based on Array.prototype.reverse
+ * 
+ * @param {Array} array The array to invert the elements of
+ * @return {Array} The new array with the elements inverted
+ */
+function invert(array) {
+ var result=[]
+ for(var i=0; Math.floor(i<array.length/2);i++){
+    var init= array[i]
+    var end= array[array.length -1 -i]
+
+    result[i]= end
+    result[array.length -1 -i]= init
+    
+    if(array.length %2 === 1){
+        result [Math.floor(array.length/2)] = array[Math.floor(array.length/ 2)]
+    }
+ }
+ return result
+}
+  
+
+
+// tests
+
+// case 0
+
+var nums = [10, 20]
+var results = invert(nums)
+console.log(results) // [20, 10]
+
+// case 1
+
+var nums = [10, 20, 30]
+var results = invert(nums)
+console.log(results) // [30, 20, 10]
+
+// case 2
+
+var nums = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+var results = invert(nums)
+console.log(results) // ['i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
