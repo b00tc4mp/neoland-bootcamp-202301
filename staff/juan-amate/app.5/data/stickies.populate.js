5808779fe428df26dce0890345139b45e0c040f3
amat3 <121510770+amat3@users.noreply.github.com>
2023-01-25 16:44:43 +0100

    add utils to app; update arrays

---
+++ staff/juan-amate/app.5/data/stickies.populate.js
@@ -0,0 +1,23 @@
+var sticky = {
+    text: "hola mundo",
+    visibility: "private",
+    user: "pepito@grillo.com",
+  };
+  
+  stickies.push(sticky);
+  
+  var sticky = {
+    text: "hello world",
+    visibility: "public",
+    user: "wendy@darling.com",
+  };
+  
+  stickies.push(sticky);
+  
+  var sticky = {
+    text: "hello world",
+    visibility: "public",
+    user: "peter@pan.com",
+  };
+  
+  stickies.push(sticky);
---
+++ staff/juan-amate/app.5/logic/retrieve-public-stickies.js
@@ -0,0 +1,17 @@
+/**
+ * Retrieves the public stickies form all users that public them
+ * 
+ * @return {array} The public stickies
+ */
+function retrievePublicStickies() {
+    var publicStickies = []
+    
+    for (var i = 0; i < stickies.length; i++){
+      var sticky = stickies[i]
+  
+      if (sticky.visibility === 'public') {
+        publicStickies.push(sticky)
+      }
+    }
+    return publicStickies.reverse()
+  }
