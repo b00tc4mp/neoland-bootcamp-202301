fb8f518ea65b8be4ea19fd0c551d4fd4dc06777a
manuelbarzi <manuelbarzi@gmail.com>
2023-01-27 11:58:22 +0100

    attach delete sticky logic to home; improve ui design

--- staff/manuel-barzi/app/components/home.js
+++ staff/manuel-barzi/app/components/home.js
@@ -1,10 +1,11 @@
 var home = {}
 
-home.view = document.querySelector('.home')
-home.addStickyButton = home.view.querySelector('button')
+home.view = document.querySelector('.home-view')
+home.logoutButton = document.querySelector('.logout-button')
+home.addStickyButton = home.view.querySelector('.add-sticky-button')
 home.view.classList.add('off')
 
-home.listPublicStickies = function() {
+home.listPublicStickies = function () {
     var ul = home.view.querySelector('ul')
     ul.innerHTML = ''
 
@@ -14,12 +15,33 @@ home.listPublicStickies = function() {
         var sticky = stickies[i]
 
         var li = document.createElement('li')
-        
+
         var p = document.createElement('p')
         p.innerText = sticky.text
         p.contentEditable = true
-        p.onkeyup = function(event) {
+        p.onkeyup = function (event) {
             console.log(event.target.innerText)
+        }
+
+        if (sticky.user === window.email) {
+            var deleteButton = document.createElement('button')
+
+            deleteButton.innerText = 'x'
+            deleteButton.id = sticky.id
+
+            deleteButton.onclick = function (event) {
+                var stickyId = event.target.id
+
+                try {
+                    deleteSticky(window.email, stickyId)
+
+                    home.listPublicStickies()
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+
+            li.appendChild(deleteButton)
         }
 
         var strong = document.createElement('strong')
@@ -32,12 +54,19 @@ home.listPublicStickies = function() {
     }
 }
 
-home.addStickyButton.onclick = function(event) {
+home.addStickyButton.onclick = function (event) {
     try {
         createSticky(window.email, '', 'public')
 
-        home.listPublicStickies()        
-    } catch(error) {
+        home.listPublicStickies()
+    } catch (error) {
         console.error(error.message)
     }
+}
+
+home.logoutButton.onclick = function(event) {
+    delete window.email
+
+    home.view.classList.add('off')
+    login.view.classList.remove('off')
 }
