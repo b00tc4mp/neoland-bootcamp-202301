d14572942fa885f37ff0e295606d901750bbc767
manuelbarzi <manuelbarzi@gmail.com>
2023-01-27 12:33:04 +0100

    add profile and update password panels; mechanice navigation to profile and home; add empty update user password logic

--- staff/manuel-barzi/app.10/components/home.js
+++ staff/manuel-barzi/app.10/components/home.js
@@ -2,18 +2,12 @@ var home = {}
 
 home.view = document.querySelector('.home-view')
 home.logoutButton = document.querySelector('.logout-button')
-home.addButton = home.view.querySelector('.add-button')
+home.addStickyButton = home.view.querySelector('.add-sticky-button')
 home.view.classList.add('off')
-home.listPanel = home.view.querySelector('.list-panel')
-// home.listPanel.classList.add('off')
-home.profilePanel = home.view.querySelector('.profile-panel')
-home.profilePanel.classList.add('off')
-home.updatePasswordPanel = home.profilePanel.querySelector('.update-password-panel')
-home.profileLink = home.view.querySelector('.profile-link')
-home.logoLink = home.view.querySelector('.logo-link')
 
 home.listPublicStickies = function () {
-    home.listPanel.innerHTML = ''
+    var ul = home.view.querySelector('ul')
+    ul.innerHTML = ''
 
     var stickies = retrievePublicStickies()
 
@@ -56,11 +50,11 @@ home.listPublicStickies = function () {
         li.appendChild(p)
         li.appendChild(strong)
 
-        home.listPanel.appendChild(li)
+        ul.appendChild(li)
     }
 }
 
-home.addButton.onclick = function (event) {
+home.addStickyButton.onclick = function (event) {
     try {
         createSticky(window.email, '', 'public')
 
@@ -75,34 +69,4 @@ home.logoutButton.onclick = function(event) {
 
     home.view.classList.add('off')
     login.view.classList.remove('off')
-}
-
-home.profileLink.onclick = function(event) {
-    event.preventDefault()
-
-    home.listPanel.classList.add('off')
-    home.addButton.classList.add('off')
-    home.profilePanel.classList.remove('off')
-}
-
-home.logoLink.onclick = function(event) {
-    event.preventDefault()
-
-    home.profilePanel.classList.add('off')
-    home.listPanel.classList.remove('off')
-    home.addButton.classList.remove('off')
-}
-
-home.updatePasswordPanel.onsubmit = function(event) {
-    event.preventDefault()
-
-    var currentPassword = event.target.currentPassword.value
-    var newPassword = event.target.newPassword.value
-    var newPasswordConfirm = event.target.newPasswordConfirm.value
-
-    try {
-        updateUserPassword(window.email, currentPassword, newPassword, newPasswordConfirm)
-    } catch(error) {
-        console.error(error)
-    }
 }
---
+++ staff/manuel-barzi/app/logic/update-user-password.js
@@ -0,0 +1,11 @@
+/**
+ * Updates the user password
+ * 
+ * @param {string} email The user email
+ * @param {string} currentPassword The user current password
+ * @param {string} newPassword The user new password
+ * @param {string} newPasswordConfirm The confirmation of the new password
+ */
+function updateUserPassword(email, currentPassword, newPassword, newPasswordConfirm) {
+    // TODO
+}
