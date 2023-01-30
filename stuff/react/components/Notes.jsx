21ff211eb4864c3288ce497205f5431f449a794f
manuelbarzi <manuelbarzi@gmail.com>
2023-01-30 15:00:05 +0100

    add notes comopnent challenge

---
+++ stuff/react/components/Notes.jsx
@@ -0,0 +1,13 @@
+/*
+TODO create a component that lists notes entered manually via a form
+*/
+
+function Notes() {
+    const [notes, setNotes] = React.useState([])
+
+    return <div>
+        <form></form>
+        
+        <ul></ul>
+    </div>
+}
