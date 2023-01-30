833640aef48cc194c530d7225d9e16fa7c23e6a2
manuelbarzi <manuelbarzi@gmail.com>
2023-01-30 11:58:39 +0100

    add more react demos

--- stuff/react/main.jsx
+++ stuff/react/main.jsx
@@ -1,5 +1,5 @@
 const h1 = <h1 className="text-red" title="hello world">hola mundo</h1>
-      
+
 const ul = <ul>
         <li>red</li>
         <li>green</li>
@@ -10,4 +10,14 @@ const ul = <ul>
 
 const container = document.querySelector('#root')
 const root = ReactDOM.createRoot(container)
-root.render([h1, ul])
+root.render([
+        h1, 
+        ul, 
+        <Calculin />, 
+        <Hello />, 
+        <HelloTo who="Lau" />, 
+        <HelloTo who="Flor" />, 
+        <HelloTo who="Eli" />,
+        <SaluteTo salutation="Bye" who="Xavi" />,
+        <SaluteTo salutation="Ciao" who="Juan" />
+])
