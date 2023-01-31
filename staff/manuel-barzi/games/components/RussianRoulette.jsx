c4a501d009d18c95ba48f14802e4c70c27c1a2c0
manuelbarzi <manuelbarzi@gmail.com>
2023-01-31 15:33:06 +0100

    add more todos to russian roulette game

--- staff/manuel-barzi/games/components/RussianRoulette.jsx
+++ staff/manuel-barzi/games/components/RussianRoulette.jsx
@@ -1,8 +1,15 @@
 function RussianRoulette() {
     // TODO state for bullet (random from 1 to 6)
+    // HINT Math.round(Math.random() * 5) + 1
     // TODO state for shoot (from 0)
+
+    // TODO handleShoot
+
+    // TODO handleRestart
+
     return <div>
         😅😢 💦 🔫
         <button>shoot</button>
+        <button>restart</button>
     </div>
 }
