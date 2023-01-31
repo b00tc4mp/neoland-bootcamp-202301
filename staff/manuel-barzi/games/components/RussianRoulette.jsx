326313be4a1cde351ae7ff96fbcddb0b7ea7fcf8
manuelbarzi <manuelbarzi@gmail.com>
2023-01-31 16:37:33 +0100

    add solution to russian roulette game

--- staff/manuel-barzi/games/components/RussianRoulette.jsx
+++ staff/manuel-barzi/games/components/RussianRoulette.jsx
@@ -1,15 +1,28 @@
 function RussianRoulette() {
+    console.log('RussianRoulette -> render');
+
     // TODO state for bullet (random from 1 to 6)
     // HINT Math.round(Math.random() * 5) + 1
+    const [bullet, setBullet] = React.useState(Math.round(Math.random() * 5) + 1)
+
     // TODO state for shoot (from 0)
+    const [shoot, setShoot] = React.useState(0)
 
     // TODO handleShoot
+    const handleShoot = () => {
+        if (shoot !== bullet)
+            setShoot(shoot + 1)
+    }
 
     // TODO handleRestart
+    const handleRestart = () => {
+        setBullet(Math.round(Math.random() * 5) + 1)
+        setShoot(0)
+    }
 
     return <div>
-        😅😢 💦 🔫
-        <button>shoot</button>
-        <button>restart</button>
+        <p>{shoot !== bullet ? '😅' : '😢 💦'} 🔫 {shoot}</p>
+        <button onClick={handleShoot}>shoot</button>
+        <button onClick={handleRestart}>restart</button>
     </div>
 }
