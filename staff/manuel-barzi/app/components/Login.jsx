991e72566be97a45022d15c27cb5d5762496fe47
manuelbarzi <manuelbarzi@gmail.com>
2023-02-01 16:13:23 +0100

    add login to app react

--- staff/manuel-barzi/app/components/Login.jsx
+++ staff/manuel-barzi/app/components/Login.jsx
@@ -1,45 +1,36 @@
-function Register() {
-    console.log('Register -> render')
+function Login() {
+    console.log('Login -> render')
 
     const [feedback, setFeedback] = React.useState('')
 
     const handleSubmit = event => {
         event.preventDefault()
 
-        const name = event.target.name.value
-        const age = event.target.age.value
         const email = event.target.email.value
         const password = event.target.password.value
 
+        //console.log(email, password)
         try {
-            registerUser(name, age, email, password)
+            authenticateUser(email, password)
             setFeedback('')
-            //alert('user registration successful')
-            // TODO navigate to login
+            console.log('ok, logged in')
         } catch(error) {
-            //alert(error.message)
             setFeedback(error.message)
         }
     }
 
-    return <main className="register">
-        <h1>Register</h1>
+    return <main className="login">
+        <h1>Login</h1>
         <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input type="name" id="name" placeholder="input your name" />
-
-            <label htmlFor="age">Age</label>
-            <input type="number" id="age" placeholder="input your age" />
-
             <label htmlFor="email">E-mail</label>
             <input type="email" id="email" placeholder="input your e-mail" />
 
             <label htmlFor="password">Password</label>
             <input type="password" id="password" placeholder="input your password" />
 
-            <button type="submit">Register</button>
+            <button type="submit">Login</button>
         </form>
         <p className="feedback">{feedback}</p>
-        <p>or <a href="">Login</a></p>
+        <p>or <a href="">Register</a></p>
     </main>
 }
