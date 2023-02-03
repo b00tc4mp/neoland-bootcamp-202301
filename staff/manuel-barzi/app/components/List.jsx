ab6c5887348e754af70736043bb48f05728d475a
manuelbarzi <manuelbarzi@gmail.com>
2023-02-03 10:33:44 +0100

    make user stickies editable and call logic

--- staff/manuel-barzi/app/components/List.jsx
+++ staff/manuel-barzi/app/components/List.jsx
@@ -9,9 +9,17 @@ function List() {
         alert(error.message)
     }
 
+    const handleUpdateText = event => {
+        try {
+            updateStickyText(sessionStorage.email, event.target.id, event.target.innerText)
+        } catch(error) {
+            alert(error.message)
+        }
+    }
+
     return <ul className="list-panel">
         {stickies.map(sticky => <li key={sticky.id}>
-            <p>{sticky.text}</p>
+            <p id={sticky.id} contentEditable={sticky.user === sessionStorage.email? true : false} onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p>
             <strong>{sticky.user}</strong>
         </li>)}
     </ul>
