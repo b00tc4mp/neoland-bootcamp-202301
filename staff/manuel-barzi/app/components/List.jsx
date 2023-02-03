a909e72bcc66856cfa14e960b132f0942f8e327e
manuelbarzi <manuelbarzi@gmail.com>
2023-02-03 15:58:07 +0100

    add visibility functionality in list

--- staff/manuel-barzi/app/components/List.jsx
+++ staff/manuel-barzi/app/components/List.jsx
@@ -1,6 +1,8 @@
 function List() {
     console.log('List -> render')
 
+    const [updateStamp, setUpdateStamp] = React.useState(Date.now())
+
     let stickies
 
     try {
@@ -17,8 +19,30 @@ function List() {
         }
     }
 
+    const handleDelete = event => {
+        try {
+            deleteSticky(sessionStorage.email, event.target.id)
+            setUpdateStamp(Date.now())
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    const handleUpdateVisibility = event => {
+        try {
+            updateStickyVisibility(sessionStorage.email, event.target.id, event.target.dataset.visibility === 'public' ? 'private' : 'public')
+            setUpdateStamp(Date.now())
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
     return <ul className="list-panel">
         {stickies.map(sticky => <li key={sticky.id}>
+            {sticky.user === sessionStorage.email && <button id={sticky.id} data-visibility={sticky.visibility} onClick={handleUpdateVisibility}>+/-</button>}
+
+            {sticky.user === sessionStorage.email && <button id={sticky.id} onClick={handleDelete}>x</button>}
+
             {
                 // sticky.user === sessionStorage.email ?
                 //     <p id={sticky.id} contentEditable onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p>
@@ -27,6 +51,7 @@ function List() {
             }
             {/* <p id={sticky.id} contentEditable={sticky.user === sessionStorage.email? true : false} onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p> */}
             <p id={sticky.id} contentEditable={sticky.user === sessionStorage.email} onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p>
+
             <strong>{sticky.user}</strong>
         </li>)}
     </ul>
