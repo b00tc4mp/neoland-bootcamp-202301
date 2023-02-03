6e8ce549a3e9bc32b2331735f43a4fa7e438693d
manuelbarzi <manuelbarzi@gmail.com>
2023-02-03 11:32:48 +0100

    adjust sticky edition render

--- staff/manuel-barzi/app/components/List.jsx
+++ staff/manuel-barzi/app/components/List.jsx
@@ -5,21 +5,28 @@ function List() {
 
     try {
         stickies = retrievePublicStickies()
-    } catch(error) {
+    } catch (error) {
         alert(error.message)
     }
 
     const handleUpdateText = event => {
         try {
             updateStickyText(sessionStorage.email, event.target.id, event.target.innerText)
-        } catch(error) {
+        } catch (error) {
             alert(error.message)
         }
     }
 
     return <ul className="list-panel">
         {stickies.map(sticky => <li key={sticky.id}>
-            <p id={sticky.id} contentEditable={sticky.user === sessionStorage.email? true : false} onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p>
+            {
+                // sticky.user === sessionStorage.email ?
+                //     <p id={sticky.id} contentEditable onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p>
+                //     :
+                //     <p id={sticky.id}>{sticky.text}</p>
+            }
+            {/* <p id={sticky.id} contentEditable={sticky.user === sessionStorage.email? true : false} onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p> */}
+            <p id={sticky.id} contentEditable={sticky.user === sessionStorage.email} onKeyUp={handleUpdateText} suppressContentEditableWarning={true}>{sticky.text}</p>
             <strong>{sticky.user}</strong>
         </li>)}
     </ul>
