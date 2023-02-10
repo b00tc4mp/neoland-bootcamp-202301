eb71c792f19386939dc91dd0926a9b26eb8f8cac
Florencia-Mussis <mussisflorencia@gmail.com>
2023-02-10 17:33:17 +0100

    cocktails

---
+++ staff/florencia-mussis/cocktails/App.jsx
@@ -0,0 +1,50 @@
+function App() {
+    const [items, setItems] = React.useState([])
+    const [itemShown, setItemShown] = React.useState()
+    const [view, setView] = React.useState('search')
+
+    const handleSearch = event => {
+        event.preventDefault()
+        const query = event.target.query.value
+
+        searchCocktails(query, results => setItems(results))
+    }
+
+    const handleDrinkClick = id => {
+        // lanzar una llamada para recuperar de la api la información del drink en particular
+        // asíncrona
+        retrieveCocktail(id, result => {
+            setItemShown(result) 
+            
+            setView("drink")
+        })
+    }
+
+    return <div>
+    {view === "search" && <div>
+        <form onSubmit={handleSearch}>
+            <input type="search" name="query"></input>
+            <button type="submit">Drunk</button>
+        </form>
+        <ul>{items.map(item =>
+            <li key={item.idDrink} onClick={() => handleDrinkClick(item.idDrink)}>
+                <img src={item.strDrinkThumb} />
+                <h2>{item.strDrink}</h2>
+            </li>)}
+        </ul>
+    </div>}
+    {view === "drink" && <div>
+        <ul>
+            <li>
+                <img src={itemShown.strDrinkThumb} />
+                <h2>{itemShown.strDrink}</h2>
+                <p>Alcohol:{itemShown.strAlcoholic}</p>
+                <p>ingredients:</p>
+                <ul>{itemShown.ingredients.map(ingredient =>  <li>{ingredient}</li>)}
+               </ul>
+                <p>Instructions:{itemShown.strInstructions}</p>
+            </li>
+        </ul>
+    </div>}
+    </div>
+}
--- staff/florencia-mussis/giphy.1/main.js
+++ staff/florencia-mussis/giphy.1/main.js
@@ -1,4 +1,4 @@
-document.body.innerHTML = "";
+document.body.innerHTML = ""; //vacia el html
 
 var form = document.createElement("form");
 form.classList.add("w-2/6","h-16", "rounded-lg","border-2", "m-8", "flex", "items-center", "justify-center","border-grey", "gap-4");
@@ -16,7 +16,7 @@ form.onsubmit = (event) => {
     images[i].remove();
   }
 
-  var query = event.target.q.value;
+  var query = event.target.q.value; //valor q completo en el formulario
 
   searchGiphy(query, (result) => {
     var imagesContainer = document.createElement("div")
@@ -24,11 +24,18 @@ form.onsubmit = (event) => {
     imagesContainer.classList.add("flex", "flex-wrap", "gap-2","justify-center", "items-center")
 
     result.data.forEach((item) => {
+      //Guardo la URL de UN item en una variable
       var url = item.images.original.url;
 
+      //creo un html img
       var img = document.createElement("img");
+
+      //Defino para ese img la propiedad src=variable_URL
       img.src = url;
+
+      //Añado clases tailwind al img
       img.classList.add("giphy-image", "border-2", "w-1/4", "h-2/12", "items-center");
+
 
       imagesContainer.append(img);
     });
@@ -41,8 +48,6 @@ form.onsubmit = (event) => {
 
 var input = document.createElement("input");
 input.classList.add("w-2/5")
-
-
 input.type = "search";
 input.name = "q";
 input.placeholder = "Busca en Giphy";
