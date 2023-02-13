0d48743d650e7a5c9ea40a881cd6345e6f8b8cf4
xavierVinas <xavivinass@gmail.com>
2023-02-13 19:49:44 +0100

    add comments to cocktails 2.0

--- staff/xavier-vinas/cocktails2.0/App.jsx
+++ staff/xavier-vinas/cocktails2.0/App.jsx
@@ -3,9 +3,9 @@ function App(){
     const [query, setQuery]= React.useState()
     const [itemId, setItemId]= React.useState()
 
-    const handleQuery = query => {
-        setQuery(query)
-        setView("list")
+    const handleQuery = query => { // recibe el valor del input
+        setQuery(query) // seteamos el estado de query y le actualizamos a la query del input
+        setView("list") // la vista va a list 
     }
 
     const handleDetail = itemId => {
@@ -16,8 +16,8 @@ function App(){
     return <div>
     <Search onQuery={handleQuery} />
 
-    {view === 'list' && <List query={query} onItemClick={handleDetail} />}
+    {view === 'list' && <List query={query} onItemClick={handleDetail} />} {/* si la vista es list renderiza list */}
 
-    {view === 'detail' && <Detail itemId={itemId} />}
+    {view === 'detail' && <Detail itemId={itemId} />} {/* si la vista es detail renderiza detail  */}
 </div>
 }
--- staff/xavier-vinas/cocktails2.0/components/List.jsx
+++ staff/xavier-vinas/cocktails2.0/components/List.jsx
@@ -1,19 +1,17 @@
 function List({query, onItemClick}){
 
-    const [items, setItems]= React.useState([])
+    const [items, setItems]= React.useState([]) // arranca el array vacio
 
     React.useEffect(()=>{
         searchCocktails(query,results =>{
             setItems(results)
         })
-    }, [query])
+    }, [query]) // que pinte cuando tenga los resultados , es asincrono . se ejecuta cuando renderiza la pagina y cuando hay un cambio en query
 
     const handleItemClick = event =>{
         const itemId = event.currentTarget.dataset.id
-        onItemClick(itemId)
-        
+        onItemClick(itemId)        
     }
-    
     return <div>
         {items && <ul>{items.map(item =>
             <li key={item.idDrink} data-id={item.idDrink} className="" onClick={handleItemClick}>
