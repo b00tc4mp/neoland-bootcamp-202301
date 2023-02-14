c7519492bd1430a7ec0194d7f2090e528122e020
LauraTigreCarrasco <lauratigre@gmail.com>
2023-02-14 15:07:25 +0100

    add to  baby steps file notes and show

--- staff/laura-tigre/baby-steps/render.js
+++ staff/laura-tigre/baby-steps/render.js
@@ -11,17 +11,17 @@ const b = JSON.parse(a)
 
 let result = ''
 
-for(let i=0 ; i< b.length; i++){
-    const element= b[i]
-    for(let i2=0; i2< element.length ;i2++){
-        const element2= element[i2]
-        if(element2===1){
-            result= result + '▮'
-        }else{
-            result= result + ' '
-        }
-
+for (let i = 0; i < b.length; i++) {
+    const element = b[i]
+    for (let j = 0; j < element.length; j++) {
+        const element2 = element[j]
+        // if (element2 === 1) {
+        //     result = result + '▮'
+        // } else {
+        //     result = result + ' '
+        // }
+        result += element2?'▮': ' '
     }
-    result= result + '\n'
+    result = result + '\n'
 }
 console.log(result)
