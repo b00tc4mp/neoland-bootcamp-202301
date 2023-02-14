25795a7a998ec9cd43b7fcdb26fb547a47a08e69
Florencia-Mussis <mussisflorencia@gmail.com>
2023-02-14 18:04:03 +0100

    add notes

--- staff/florencia-mussis/baby-steps/notes.js
+++ staff/florencia-mussis/baby-steps/notes.js
@@ -11,7 +11,7 @@
 // note updated (note-1676382448483.txt)
 
 // DEMO add more content to existing one (with line break)
-// $ node notes add note-1676382448483 'ciao mondo'
+// $ node notes add-text note-1676382448483 'ciao mondo'
 // note updated (note-1676382448483.txt)
 
 // DEMO delete a note
@@ -27,27 +27,52 @@
 // note-1676383832973
 // note-1676385834604
 
+// operation= command = metodo
 
 const [, , operation] = process.argv
 
 const fs = require('fs')
 
 if (operation === 'add') {
-    const content = process.argv[3]
-    
-    const { writeFile } = fs
+    if (process.argv.length === 4) {
+        const content = process.argv[3]
 
-    const file = 'note-' + Date.now() + '.txt'
+        const { writeFile } = fs
 
-    writeFile(file, content, 'utf8', error => {
-        if (error) {
-            console.error('could not write note, because of error: ' + error.message)
+        const file = 'note-' + Date.now() + '.txt'
 
-            return
-        }
+        writeFile(file, content, 'utf8', error => {
+            if (error) {
+                console.error('could not write note, because of error: ' + error.message)
 
-        console.log('note created (' + file + ')')
-    })
+                return
+            }
+
+            console.log('note created (' + file + ')')
+        })
+    } else if (process.argv.length === 5) {
+        const noteId = process.argv[3]
+        const newContent = process.argv[4]
+        const { readFile, writeFile } = fs
+
+        const file = noteId + '.txt'
+
+        readFile(file, 'utf8', (error, previousContent) => {
+            if (error) {
+                console.error('could not read note, because of error: ' + error.message)
+
+                return
+            }
+            const allContent = previousContent + '\n' + newContent
+            writeFile(file, allContent, 'utf8', error => {
+                if (error) {
+                    console.error('could not write note, becaise of error: ' + error.message)
+                    return
+                }
+            })
+            console.log('note (' + file + ')updated')
+        })
+    }
 } else if (operation === 'get') {
     const noteId = process.argv[3]
 
@@ -64,5 +89,51 @@ if (operation === 'add') {
 
         console.log(content)
     })
+} else if (operation === 'set') {
+    const noteId = process.argv[3]
+
+    const content = process.argv[4]
+
+    const { writeFile } = fs
+
+    const file = noteId + '.txt'
+
+    writeFile(file, content, 'utf8', error => {
+        if (error) {
+            console.error('could not write note, becaise of error: ' + error.message)
+            return
+        }
+        console.log('note (' + file + ')updated')
+    })
+   
+} else if (operation === 'delete'){
+    const noteId = process.argv[3]
+    const {unlink} = fs
+    const file = noteId + '.txt'
+
+    unlink(file, error => {
+        if (error) {
+            console.error('could not delete note, becaise of error: ' + error.message)
+            return
+        }
+        console.log ('note (' + file + ')deleted')
+    } )
+} else if (operation === 'list'){
+    const folder = process.argv[3]
+    const {readdir} = fs
+
+    readdir(folder, (error, files)=>{
+        if (error) {
+            console.error('could not found files, becaise of error: ' + error.message)
+            return
+        }
+        for (var i= 0; i < files.length; i++){
+            const element = files[i]
+            if (element.slice(-4) === '.txt'){
+                console.log(element)
+            }
+        }
+    })
 }
 
+//node baby-steps/notes list baby-steps
