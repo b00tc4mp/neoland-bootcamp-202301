246ae83a7b77b68c950d2b1566692f69b36b5bf1
manuelbarzi <manuelbarzi@gmail.com>
2023-02-14 15:51:56 +0100

    add node baby steps challenges

---
+++ staff/manuel-barzi/baby-steps/notes.js
@@ -0,0 +1,68 @@
+// DEMO
+// $ node notes add "hola mundo"
+// note created (note-131313412343.txt)
+
+// DEMO
+// $ node notes get note-1676382448483
+// hola mundo
+
+// DEMO replace content with new one
+// $ node notes set note-1676382448483 'ciao mondo'
+// note updated (note-1676382448483.txt)
+
+// DEMO add more content to existing one (with line break)
+// $ node notes add note-1676382448483 'ciao mondo'
+// note updated (note-1676382448483.txt)
+
+// DEMO delete a note
+// $ node notes delete note-1676382448483
+// note deleted (note-1676382448483.txt)
+
+// DEMO list notes
+// $ node notes list
+// note-1676382448483
+// note-1676382467270
+// note-1676382496443
+// note-1676383826688
+// note-1676383832973
+// note-1676385834604
+
+
+const [, , operation] = process.argv
+
+const fs = require('fs')
+
+if (operation === 'add') {
+    const content = process.argv[3]
+    
+    const { writeFile } = fs
+
+    const file = 'note-' + Date.now() + '.txt'
+
+    writeFile(file, content, 'utf8', error => {
+        if (error) {
+            console.error('could not write note, because of error: ' + error.message)
+
+            return
+        }
+
+        console.log('note created (' + file + ')')
+    })
+} else if (operation === 'get') {
+    const noteId = process.argv[3]
+
+    const { readFile } = fs
+
+    const file = noteId + '.txt'
+
+    readFile(file, 'utf8', (error, content) => {
+        if (error) {
+            console.error('could not read note, because of error: ' + error.message)
+
+            return
+        }
+
+        console.log(content)
+    })
+}
+
