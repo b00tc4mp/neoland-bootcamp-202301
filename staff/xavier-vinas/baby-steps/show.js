c520c11968bb17ac976c96df1450be1c25a6e468
xavierVinas <xavivinass@gmail.com>
2023-02-14 16:27:38 +0100

    add node bb steps

---
+++ staff/xavier-vinas/baby-steps/show.js
@@ -0,0 +1,20 @@
+// Demo 
+// node show hell0world.js
+//...
+
+
+const file = process.argv[2]
+
+const fs = require("fs")
+
+const { readFile } = fs
+
+readFile(file, "utf8", (error, content) => {
+    if(error) {
+        console.error("could not read file , because " + error.message)
+
+        return
+    }
+
+    console.log(content)
+})
