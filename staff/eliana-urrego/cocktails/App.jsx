41dea5f610ea44dd60a69d9c5abe7f1023b96701
EliUrrego <elianaurrego18@gmail.com>
2023-02-14 09:18:30 +0100

    add cocktails and giphies

---
+++ staff/eliana-urrego/cocktails/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+    console.log('App -> render')
+
+    const [view, setView] = React.useState('list')
+    const [query, setQuery] = React.useState()
+    const [itemId, setItemId] = React.useState()
+
+    const handleQuery = query => {
+        setQuery(query)
+        setView('list')
+
+    }
+
+    const handleDetail = itemId => {
+        setItemId(itemId)
+        setView('detail')
+    }
+
+    return <div>
+        <Search onQuery={handleQuery} />
+
+        {view === 'list' && <List query={query} onItemClick={handleDetail} />}
+
+        {view === 'detail' && <Detail itemId={itemId} />}
+    </div>
+}
