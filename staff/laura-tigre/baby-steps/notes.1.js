f9d6df39dc72cd57983f2d6880fe199251650ac0
LauraTigreCarrasco <lauratigre@gmail.com>
2023-02-14 16:28:58 +0100

    create folder notes to add notes and retrieve note

--- staff/laura-tigre/baby-steps/notes.1.js
+++ staff/laura-tigre/baby-steps/notes.1.js
@@ -3,13 +3,10 @@ const [, , command, content]= process.argv
 
 const fs= require('fs')
 
-
 if(command=== 'add'){
-    const content= process.argv[3]
     const{writeFile}=fs
     const file = 'note-' +Date.now() + '.txt'
-    
-   
+
 
     writeFile(file,content, 'utf8', error=>{
         if(error){
@@ -17,18 +14,5 @@ if(command=== 'add'){
             return
         }
         console.log('note created (' +file + ')')
-    })
-    
-
-}else if (command==='get'){
-    const noteId= process.argv[3]
-    const{readFile}= fs
-    const file = noteId +'.txt'
-    readFile(file, 'utf8',(error, content)=>{
-        if(error){
-            console.error('could not read file, because '+ error.message)
-            return
-        }
-        console.log(content)
     })
 }
