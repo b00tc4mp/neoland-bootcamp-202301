e1d41dd20f65b0a2f2d3a5b8360416c3a7c9be07
manuelbarzi <manuelbarzi@gmail.com>
2023-02-15 15:57:14 +0100

    add little refactor in register user test to chain test cases; add directives for the next test case (unhappy)

--- staff/manuel-barzi/api/logic/registerUser.test.js
+++ staff/manuel-barzi/api/logic/registerUser.test.js
@@ -5,37 +5,60 @@ const checkFileExists = require('../utils/checkFileExists')
 
 // case 0
 
-deleteAllFilesFromDirectory('data/users', error => {
-    if (error) {
-        console.error(error.message)
-
-        return
-    }
-
-    const name = 'Marie Curie'
-    const age = 87
-    const email = 'marie@curie.com'
-    const password = '123123123'
-
-    registerUser(name, age, email, password, (error, userId) => {
+function case0(done) {
+    deleteAllFilesFromDirectory('data/users', error => {
         if (error) {
             console.error(error.message)
-
+    
             return
         }
-
-        const fileName = userId + '.json'
-
-        const filePath = 'data/users/' + fileName
-
-        checkFileExists(filePath, (error, exists) => {
+    
+        const name = 'Marie Curie'
+        const age = 87
+        const email = 'marie@curie.com'
+        const password = '123123123'
+    
+        registerUser(name, age, email, password, (error, userId) => {
             if (error) {
                 console.error(error.message)
-
+    
                 return
             }
+    
+            const fileName = userId + '.json'
+    
+            const filePath = 'data/users/' + fileName
+    
+            checkFileExists(filePath, (error, exists) => {
+                if (error) {
+                    console.error(error.message)
+    
+                    return
+                }
+    
+                verify(exists)
 
-            verify(exists)
+                done()
+            })
         })
+    })
+}
+
+// TODO unhappy test case: register fails because user already registered
+function case1(done) {
+    // 1. clean db
+    // 2. create with writeFile a user in data/users
+    // 3. call registerUser logic (you will run this in the previous writeFile callback)
+    // 4. verify that registerUser sent and error to its callback (run this in registerUser callback)
+
+    verify(true)
+
+    done()
+}
+
+case0(() => {
+    case1(() => {
+        // ...)
+        console.log('end')
     })
 })
