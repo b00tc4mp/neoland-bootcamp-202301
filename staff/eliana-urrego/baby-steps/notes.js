3f56811cd989626f65cc9c2cceb247bfa46260ab
EliUrrego <elianaurrego18@gmail.com>
2023-02-15 09:25:21 +0100

    add notes

--- staff/eliana-urrego/baby-steps/notes.js
+++ staff/eliana-urrego/baby-steps/notes.js
@@ -33,22 +33,50 @@ const [, , operation] = process.argv
 const fs = require('fs')
 
 if (operation === 'add') {
-    if (process.argv.length === 4) { }
-    const content = process.argv[3]
+    if (process.argv.length === 4) {
+        const content = process.argv[3]
 
-    const { writeFile } = fs
+        const { writeFile } = fs
 
-    const file = 'note-' + Date.now() + '.txt'
+        const file = 'note-' + Date.now() + '.txt'
 
-    writeFile(file, content, 'utf8', error => {
-        if (error) {
-            console.error('could not write note, because of error: ' + error.message)
+        writeFile(file, content, 'utf8', error => {
+            if (error) {
+                console.error('could not write note, because of error: ' + error.message)
 
-            return
-        }
+                return
+            }
 
-        console.log('note created (' + file + ')')
-    })
+            console.log('note created (' + file + ')')
+        })
+    } else if (process.argv.length === 5) {
+        const noteId = process.argv[3]
+        const newContent = process.argv[4]
+
+        const { readFile, writeFile } = fs
+
+        const file = noteId + '.txt'
+
+        readFile(file, 'utf8', (error, previousContent) => {
+            if (error) {
+                console.error('could not read note, because of error: ' + error.message)
+
+                return
+            }
+            const allContent = previousContent + '\n' + newContent
+
+            writeFile(file, content, 'utf8', error => {
+                if (error) {
+                    console.error('could not modify this note, because of error: ' + error.message)
+
+                    return
+                }
+
+            })
+            console.log('note (' + file + ') updated')
+
+        })
+    }
 } else if (operation === 'get') {
     const noteId = process.argv[3]
 
