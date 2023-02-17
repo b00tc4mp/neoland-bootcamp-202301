5cd00f8a283487f73555139d23851729d75aa7de
amat3 <121510770+amat3@users.noreply.github.com>
2023-02-17 17:58:52 +0100

    add unregisterUser logic and test

--- staff/juan-amate/api/logic/registerUser.test.js
+++ staff/juan-amate/api/logic/registerUser.test.js
@@ -44,67 +44,67 @@ describe('registerUser', () => {
             })
         })
     })
-})
 
-it('succeeds for a new user and others already exist', done => {
-    deleteAllFilesFromDirectory('data/users', error => {
-        if (error) {
-            console.error(error.message)
-
-            return
-        }
-
-        const name = 'Albert Einstein'
-        const age = 97
-        const email = 'albert@einstein.com'
-        const password = '123123123'
-
-        const user = { name, age, email, password }
-
-        const userJson = JSON.stringify(user) // { "name": "Marie Curie", "age": 87, "email": "marie@curie.com", "password": "123123123" }
-
-        const { writeFile } = fs
-
-        const userId = 'user-' + Date.now()
-
-        const file = userId + '.json'
-
-        const userFilePath = 'data/users/' + file
-
-        writeFile(userFilePath, userJson, 'utf8', error => {
+    it('succeeds for a new user and others already exist', done => {
+        deleteAllFilesFromDirectory('data/users', error => {
             if (error) {
                 console.error(error.message)
 
                 return
             }
 
-            const name = 'Marie Curie'
-            const age = 87
-            const email = 'marie@curie.com'
+            const name = 'Albert Einstein'
+            const age = 97
+            const email = 'albert@einstein.com'
             const password = '123123123'
 
-            registerUser(name, age, email, password, (error, userId) => {
+            const user = { name, age, email, password }
+
+            const userJson = JSON.stringify(user) // { "name": "Marie Curie", "age": 87, "email": "marie@curie.com", "password": "123123123" }
+
+            const { writeFile } = fs
+
+            const userId = 'user-' + Date.now()
+
+            const file = userId + '.json'
+
+            const userFilePath = 'data/users/' + file
+
+            writeFile(userFilePath, userJson, 'utf8', error => {
                 if (error) {
                     console.error(error.message)
 
                     return
                 }
 
-                const fileName = userId + '.json'
+                const name = 'Marie Curie'
+                const age = 87
+                const email = 'marie@curie.com'
+                const password = '123123123'
 
-                const filePath = 'data/users/' + fileName
-
-                checkFileExists(filePath, (error, exists) => {
+                registerUser(name, age, email, password, (error, userId) => {
                     if (error) {
                         console.error(error.message)
 
                         return
                     }
 
-                    //verify(exists)
-                    expect(exists).to.be.true
+                    const fileName = userId + '.json'
 
-                    done()
+                    const filePath = 'data/users/' + fileName
+
+                    checkFileExists(filePath, (error, exists) => {
+                        if (error) {
+                            console.error(error.message)
+
+                            return
+                        }
+
+                        //verify(exists)
+                        expect(exists).to.be.true
+
+                        done()
+                    })
                 })
             })
         })
