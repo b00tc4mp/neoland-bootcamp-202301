930bc13bb3478e6baa3c9f4c9f74e5a4e53d9185
manuelbarzi <manuelbarzi@gmail.com>
2023-02-23 12:18:58 +0100

    add create sticky and retrieve public stickies logics

---
+++ staff/manuel-barzi/api/logic/retrievePublicStickies.js
@@ -0,0 +1,12 @@
+/**
+ * Retrieves the public stickies from all users that publish them
+ * 
+ * @return {Array} The public stickies
+ */
+function retrievePublicStickies() {
+    const stickies = process.db.collection('stickies')
+
+    return stickies.find({ visibility: 'public' }).toArray()
+}
+
+module.exports = retrievePublicStickies
