b427224c97872e8b2e37b223d085e400e013f164
Florencia-Mussis <mussisflorencia@gmail.com>
2023-02-27 18:12:47 +0100

    unregisterUser

--- staff/florencia-mussis/api/logic/unregisterUser.js
+++ staff/florencia-mussis/api/logic/unregisterUser.js
@@ -1,18 +1,23 @@
-const {ObjectId} = require('mongodb')
+const { ObjectId } = require('mongodb')
 
 function unregisterUser(userId, password) {
-    /* 1. read db by userId*/ 
+    /* 1. read db by userId*/
     const users = process.db.collection('users')
+    const stickies = process.db.collection('stickies')
 
-     /*2. check if user passed password  3. if not, then error*/ 
-    return users.findOne({ _id: new ObjectId(userId)})
+    /*2. check if user passed password  3. if not, then error*/
+    return users.findOne({ _id: new ObjectId(userId) })
         .then(user => {
-            if (user.password !== password) throw new Error ('wrong credentials')
+            if (user.password !== password) throw new Error('wrong credentials')
 
-               /* 4. if yes, then delete user from db and its stickies*/ 
-       
-            return users.deleteOne({_id:new ObjectId (userId)})
-        })       
+            return stickies.deleteMany({ "user": userId })
+                /* 4. if yes, then delete user from db and its stickies*/
+                .then(() => {
+                    return users.deleteOne({
+                        _id: new ObjectId(userId)
+                    })
+                })
+        })º
 }
 
 module.exports = unregisterUser
