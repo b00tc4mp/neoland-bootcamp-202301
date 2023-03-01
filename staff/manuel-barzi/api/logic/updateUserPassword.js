2052a8161d7355d9ded60ff042c94ffb5df54811
manuelbarzi <manuelbarzi@gmail.com>
2023-03-01 09:44:50 +0100

    add input validation to update user password

--- staff/manuel-barzi/api/logic/updateUserPassword.js
+++ staff/manuel-barzi/api/logic/updateUserPassword.js
@@ -1,12 +1,17 @@
 const { ObjectId } = require('mongodb')
 
-function updateUserPassword(userId, currentPassword, newPassword, newPasswordRepeat) {
-    if (currentPassword === newPassword)
-        throw new Error('current password and new password are equal')
+function updateUserPassword(userId, currentPassword, newPassword, newPasswordConfirm) {
+    if (typeof userId !== 'string') throw new Error('userId is not a string')
+    if (typeof currentPassword !== 'string') throw new Error('currentPassword is not a string')
+    if (currentPassword.length < 8) throw new Error('currentPassword is shorter than 8 characters')
+    if (typeof newPassword !== 'string') throw new Error('newPassword is not a string')
+    if (newPassword.length < 8) throw new Error('newPassword is shorter than 8 characters')
+    if (typeof newPasswordConfirm !== 'string') throw new Error('newPasswordConfirm is not a string')
+    if (newPasswordConfirm.length < 8) throw new Error('newPasswordConfirm is shorter than 8 characters')
 
-    if (newPassword !== newPasswordRepeat)
-        throw new Error('new password and new password repeat do not match')
+    if (currentPassword === newPassword) throw new Error('current password and new password are equal')
 
+    if (newPassword !== newPasswordConfirm) throw new Error('new password and new password repeat do not match')
 
     const users = process.db.collection('users')
 
