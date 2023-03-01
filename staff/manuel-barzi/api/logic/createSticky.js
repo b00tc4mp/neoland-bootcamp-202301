6ee1891477360cf85a863f3c51da96c530d6efd5
manuelbarzi <manuelbarzi@gmail.com>
2023-03-01 09:28:05 +0100

    add input validation and user logic validation

--- staff/manuel-barzi/api/logic/createSticky.js
+++ staff/manuel-barzi/api/logic/createSticky.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require('mongodb')
+
 /**
  * Creates a new sticky in the database
  * 
@@ -7,18 +9,30 @@
  */
 function createSticky(userId, text, visibility) {
     // TODO validate input arguments
+    if (typeof userId !== 'string') throw new Error('userId is not a string')
+    if (typeof text !== 'string') throw new Error('text is not a string')
+    if (typeof visibility !== 'string') throw new Error('visibility is not a string')
+    if (visibility !== 'public' && visibility !== 'private') throw new Error('visibility is not valid')
+
     // TODO validate user
+    const users = process.db.collection('users')
 
-    const stickies = process.db.collection('stickies')
+    return users.findOne({ _id: new ObjectId(userId) })
+        .then(user => {
+            if (!user) throw new Error(`user with id ${userId} not found`)
 
-    const sticky = {
-        user: userId,
-        text,
-        visibility,
-        likes: []
-    }
+            const stickies = process.db.collection('stickies')
+        
+            const sticky = {
+                user: userId,
+                text,
+                visibility,
+                likes: []
+            }
+        
+            return stickies.insertOne(sticky)
+        })
 
-    return stickies.insertOne(sticky)
 }
 
 module.exports = createSticky
--- staff/manuel-barzi/api/logic/updateStickyText.js
+++ staff/manuel-barzi/api/logic/updateStickyText.js
@@ -1,15 +1,24 @@
 const { ObjectId } = require('mongodb')
 
 function updateStickyText(userId, stickyId, text) {
-    const stickies = process.db.collection('stickies')
-    
-    return stickies.findOne({ _id: new ObjectId(stickyId) })
-        .then(sticky => {
-            if (!sticky)
-                throw new Error('sticky with id ' + stickyId + ' not found')
+    if (typeof userId !== 'string') throw new Error('userId is not a string')
+    if (typeof stickyId !== 'string') throw new Error('stickyId is not a string')
+    if (typeof text !== 'string') throw new Error('text is not a string')
 
-            if (sticky.user !== userId)
-                throw new Error('sticky with id ' + stickyId + ' does not belong to user with id ' + userId)
+    const users = process.db.collection('users')
+
+    return users.findOne({ _id: new ObjectId(userId) })
+        .then(user => {
+            if (!user) throw new Error(`user with id ${userId} not found`)
+
+            const stickies = process.db.collection('stickies')
+
+            return stickies.findOne({ _id: new ObjectId(stickyId) })
+        })
+        .then(sticky => {
+            if (!sticky) throw new Error('sticky with id ' + stickyId + ' not found')
+
+            if (sticky.user !== userId) throw new Error('sticky with id ' + stickyId + ' does not belong to user with id ' + userId)
 
             return stickies.updateOne({ _id: new ObjectId(stickyId) }, { $set: { text } })
         })
