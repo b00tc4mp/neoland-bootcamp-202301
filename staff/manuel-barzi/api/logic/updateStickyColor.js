4c36375fbe8086f772dd0624eaa2738cabe517dc
manuelbarzi <manuelbarzi@gmail.com>
2023-03-09 12:14:48 +0100

    add error types to update sticky color

--- staff/manuel-barzi/api/logic/updateStickyColor.js
+++ staff/manuel-barzi/api/logic/updateStickyColor.js
@@ -1,4 +1,4 @@
-const { validateUserId, validateStickyId, validateColor } = require('com')
+const { validateUserId, validateStickyId, validateColor, ExistenceError, CoherenceError } = require('com')
 const { User, Sticky } = require('../data/models')
 
 function updateStickyColor(userId, stickyId, color) {
@@ -8,14 +8,14 @@ function updateStickyColor(userId, stickyId, color) {
 
     return User.findById(userId)
         .then(user => {
-            if (!user) throw new Error(`user with id ${userId} not found`)
+            if (!user) throw new ExistenceError(`user with id ${userId} not found`)
 
             return Sticky.findById(stickyId)
         })
         .then(sticky => {
-            if (!sticky) throw new Error(`sticky with id ${stickyId} not found`)
+            if (!sticky) throw new ExistenceError(`sticky with id ${stickyId} not found`)
 
-            if (sticky.user.toString() !== userId) throw new Error(`sticky with id ${stickyId} does not belong to user with id ${userId}`)
+            if (sticky.user.toString() !== userId) throw new CoherenceError(`sticky with id ${stickyId} does not belong to user with id ${userId}`)
 
             sticky.color = color
 
